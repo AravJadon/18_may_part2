fd2cdf4f1cc422be64236bb342730edc5f0de37b
Arav Jadon <jadonarav2808@gmail.com>
2025-05-18 02:27:14 +0530

    first change in weak2_5.js

---
+++ weak2_5.js
@@ -0,0 +1,18 @@
+const express=require("express");
+function calculateSum(n){
+    let sum=0;
+    for(let i=0;i<n;i++) sum+=i;
+    return sum;
+}
+const app=express();
+app.get("/", (req, res) => {
+    console.log("hello i am inside");
+    const n = parseInt(req.query.n);
+    if (isNaN(n)) {
+        return res.send("Please provide a valid number using ?n=your_number");
+    }
+    res.send(calculateSum(n).toString());
+});
+
+app.listen(3000, '0.0.0.0');
+//http://192.168.219.169:3000 ---------------run this with same connected wifi system
